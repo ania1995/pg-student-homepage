8fe91cb6bd2a2d2e5543a96b1a3de9c6ffa4ee97
Anna Stanek <aniastanekk@gmail.com>
2020-11-28 20:27:08 +0100

    Change Map in Contact Modal

--- script.js
+++ script.js
@@ -62,17 +62,3 @@ function start() {
     userBox.classList.add("hidden");
   }
 }
-
-// AIzaSyAY84vwIYOY2a17gDOAZvnqg7cnvE7xWFg
-//MAP
-
-function initMap() {
-  // Map options
-  var options = {
-    zoom: 8,
-    center: { lat: 42.3601, lng: -71.0589 },
-  };
-
-  // New map
-  var map = new google.maps.Map(document.getElementById("googleMap"), options);
-}
